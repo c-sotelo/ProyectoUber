083ffdee2e7f523250eda59d56a6492f612f2fd0
Puma <133818547+Puma-Ben@users.noreply.github.com>
2024-10-25 19:11:16 -0300

    iehsajh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyChIudFU6W4rIHyrhq_E4s2KpOhOi7tW0k",
+      authDomain: "pumarinosotelo.firebaseapp.com",
+      projectId: "pumarinosotelo",
+      storageBucket: "pumarinosotelo.appspot.com",
+      messagingSenderId: "958962820482",
+      appId: "1:958962820482:web:c20d9ed4164469094edb9b"
+      }
+
 };
