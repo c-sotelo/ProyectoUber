a0ad8f08820fbfd87c64a0e781008959c7ebfec5
Puma <133818547+Puma-Ben@users.noreply.github.com>
2024-10-27 23:53:19 -0300

    mkjfdsk

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserModel } from 'src/app/models/usuario';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { HelperService } from 'src/app/services/helper.service';
+import { StorageService } from 'src/app/services/storage.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -8,47 +13,108 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  correo:string = "";
-  contrasena:string = "";
+  correo:string = "DUOC@DUOC.cl";
+  //correo:string = "tomas@tomas.cl";
+  contrasena:string = "123456";
+  token:string = "";
+  usuario:UserModel[] = [];
 
 
 
-
-
-  constructor(private router:Router) { }
+  constructor(private router:Router, 
+              private firebase:FirebaseService, 
+              private helper:HelperService,
+              private storage:StorageService,
+              private usuarioService:UsuarioService
+            
+            ) { }
 
   ngOnInit() {
-  }
-
-
-  login(){
-  
-    if (this.correo == "") {
-      alert("Ingrese un correo");
-      return;
-    }
-    if (this.contrasena == "") {
-      alert("Ingrese una contraseña");
-      return;
-    }
-    if (this.correo == "benja241104@gmail.com" && this.contrasena == 'contraseña') {
-      this.router.navigateByUrl("/inicio");
-    }else{
-      alert("Credenciales incorrectas.");
-    }
     
   }
 
 
-
+  async login(){
   
-  recuperarcontra(){
+    if (this.correo == "") {
+      this.helper.showAlert("Ingrese el correo", "Error de validación");
+      return;
+    }
+    if (this.contrasena == "") {
+      this.helper.showAlert("Ingrese la contraseña", "Error de validación");
+      return;
+    }
+    /* if (this.correo == "123" && this.contrasena == '123') {
+      this.router.navigateByUrl("/inicio");
+    }else{
+      alert("Credenciales incorrectas.");
+    } */
 
-    this.router.navigateByUrl('recuperarcontra/');
+    const loader = await this.helper.showLoader("Cargando");
+    try {
+
+      const reqFirebase = await this.firebase.login(this.correo,this.contrasena);
+      //solicitud get user
+      const token = await reqFirebase.user?.getIdToken();
+
+      if (token) {
+        this.token = token;
+        const req = await this.usuarioService.obtenerUsuario(
+          {
+            p_correo:this.correo,
+            token:token
+          }
+        );
+        this.usuario = req.data;
+        console.log("DATA USUARIO", this.usuario[0].id_usuario);
+        
+      }
+      
+      loader.dismiss();
+    } catch (error:any) {
+      
+      let msg = "Ocurrió un error al iniciar sesión.";
+      
+      if(error.code == "auth/invalid-credential1"){
+        msg = "Credenciales incorrectas.";
+      }else if(error.code == "auth/wrong-password1"){
+        msg = "Contraseña incorrecta.";
+      }else if(error.code == "auth/invalid-email1"){
+        msg = "Correo no valido.";
+      }
+
+
+      this.helper.showAlert(msg,"Aceptar");
+      loader.dismiss();
+    }
+
+    const jsonToken = 
+    [
+      {
+        "token":this.token,
+        "usuario_id":this.usuario[0].id_usuario,
+        "usuario_correo":this.usuario[0].correo_electronico
+      }
+    ];
+
+    this.storage.agregarToken(jsonToken);
+
+
+
+    //Obtenemos la info que guardamos en storage
+    let token = await this.storage.obtenerStorage();
+    console.log(token[0].usuario_correo);
+    
+    await this.helper.showToast("Login correcto!!!!!");
+    this.router.navigateByUrl("/inicio");
   }
+
+  resetPw(){
+    this.router.navigateByUrl("reset-password");
+  }
+
   registro(){
-
-    this.router.navigateByUrl('registro/');
+    this.router.navigateByUrl("registro");
   }
-  
+
 }
