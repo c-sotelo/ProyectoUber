cfe9e2308e82803199f7d4d7bc4161d49ad9ff4c
c-sotelo <135762145+c-sotelo@users.noreply.github.com>
2024-10-27 23:00:53 -0300

    animacion home inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ionViewWillEnter() {
+    const animatedImage = this.el.nativeElement.querySelector('.animated-image');
+    if (animatedImage) {
+      this.renderer.removeClass(animatedImage, 'animate'); // Quita la clase para reiniciar la animación
+      setTimeout(() => {
+        this.renderer.addClass(animatedImage, 'animate'); // Añade la clase nuevamente para activar la animación
+      }, 50); // Delay corto para asegurar el reinicio
+    }
+  }
 
 }
